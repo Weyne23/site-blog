84ce9610087599e8642a228dc9659e00cbe73ee7
Weyne Siqueira <augustoweyne12@gmail.com>
2025-09-23 18:05:52 -0300

    feature: criado card main

---
+++ src/components/FeatureSection/feature-section.tsx
@@ -0,0 +1,10 @@
+export const FeatureSection = () => {
+    return (
+        <section className="container bg-gray-700 grid gap-6 md:grid-cols-2 pb-8 pt-8 md:py-10">
+            <div className="flex flex-col gap-2 rounded-lg bg-gray-500 md:p-12">
+                <span className="text-body-tag text-blue-200 bg-blue-400 px-2 py-1 w-fit rounded-sm uppercase">Simples</span>
+                <h2 className="text-gray-100 text-heading-lg">Crie um catálogo de produtos online em poucos minutos</h2>
+            </div>
+        </section>
+    )
+}
