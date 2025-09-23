996f98afd00c52a9573ce85087de9403ad29c38c
Weyne <augustoweyne12@gmail.com>
2025-09-22 21:49:44 -0300

    feature: header da lading page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="min-h-screen min-w-full bg-slate-500">
-      <h2 className="text-4xl text-red-400">Hello Word</h2>
+      <Header />
     </div>
   );
 }
