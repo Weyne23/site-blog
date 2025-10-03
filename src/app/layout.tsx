33c959e00b7f50178a540703dc76101e2b53d88d
Weyne Siqueira <augustoweyne12@gmail.com>
2025-10-03 17:48:47 -0300

    Chore: adicionado metadados tela de posts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,24 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Site.Set',
-  description: 'Venda seus produtos como afiliado em um único lugar',
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    openGraph: {
+        title: 'Blog',
+        description: 'Dicas e estratégias para impulsionar seu negócio',
+        url: "https://site-blog-gray.vercel.app/og-image.jpg",
+        siteName: 'Blog',
+        locale: 'pt_BR',
+        type: 'website',
+        images: [
+            {
+                url: "https://site-blog-gray.vercel.app/og-image.jpg",
+                width: 800,
+                height: 600,
+                alt: 'Site.Set'
+            }
+        ]
+    }
 }
 
 export default function RootLayout({
